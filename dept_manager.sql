2b8c8d9d2b81174a238f80f8394231bc2ad4cc80
chriswecke <christopherwecke@gmail.com>
2020-10-05 17:44:36 -0500

    commit

---
+++ dept_manager.sql
@@ -0,0 +1,28 @@
+CREATE TABLE dept_manager(
+   dept_no VARCHAR(4) NOT NULL PRIMARY KEY
+  ,emp_no  INTEGER  NOT NULL
+);
+INSERT INTO dept_manager(dept_no,emp_no) VALUES ('d001',110022);
+INSERT INTO dept_manager(dept_no,emp_no) VALUES ('d001',110039);
+INSERT INTO dept_manager(dept_no,emp_no) VALUES ('d002',110085);
+INSERT INTO dept_manager(dept_no,emp_no) VALUES ('d002',110114);
+INSERT INTO dept_manager(dept_no,emp_no) VALUES ('d003',110183);
+INSERT INTO dept_manager(dept_no,emp_no) VALUES ('d003',110228);
+INSERT INTO dept_manager(dept_no,emp_no) VALUES ('d004',110303);
+INSERT INTO dept_manager(dept_no,emp_no) VALUES ('d004',110344);
+INSERT INTO dept_manager(dept_no,emp_no) VALUES ('d004',110386);
+INSERT INTO dept_manager(dept_no,emp_no) VALUES ('d004',110420);
+INSERT INTO dept_manager(dept_no,emp_no) VALUES ('d005',110511);
+INSERT INTO dept_manager(dept_no,emp_no) VALUES ('d005',110567);
+INSERT INTO dept_manager(dept_no,emp_no) VALUES ('d006',110725);
+INSERT INTO dept_manager(dept_no,emp_no) VALUES ('d006',110765);
+INSERT INTO dept_manager(dept_no,emp_no) VALUES ('d006',110800);
+INSERT INTO dept_manager(dept_no,emp_no) VALUES ('d006',110854);
+INSERT INTO dept_manager(dept_no,emp_no) VALUES ('d007',111035);
+INSERT INTO dept_manager(dept_no,emp_no) VALUES ('d007',111133);
+INSERT INTO dept_manager(dept_no,emp_no) VALUES ('d008',111400);
+INSERT INTO dept_manager(dept_no,emp_no) VALUES ('d008',111534);
+INSERT INTO dept_manager(dept_no,emp_no) VALUES ('d009',111692);
+INSERT INTO dept_manager(dept_no,emp_no) VALUES ('d009',111784);
+INSERT INTO dept_manager(dept_no,emp_no) VALUES ('d009',111877);
+INSERT INTO dept_manager(dept_no,emp_no) VALUES ('d009',111939);
---
+++ titles.sql
@@ -0,0 +1,11 @@
+CREATE TABLE titles(
+   title_id VARCHAR(5) NOT NULL PRIMARY KEY
+  ,title    VARCHAR(18) NOT NULL
+);
+INSERT INTO titles(title_id,title) VALUES ('s0001','Staff');
+INSERT INTO titles(title_id,title) VALUES ('s0002','Senior Staff');
+INSERT INTO titles(title_id,title) VALUES ('e0001','Assistant Engineer');
+INSERT INTO titles(title_id,title) VALUES ('e0002','Engineer');
+INSERT INTO titles(title_id,title) VALUES ('e0003','Senior Engineer');
+INSERT INTO titles(title_id,title) VALUES ('e0004','Technique Leader');
+INSERT INTO titles(title_id,title) VALUES ('m0001','Manager');
